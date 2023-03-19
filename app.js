91b3bad3e00a016b71ae1804d7e72573547178be
BojanIstvancic <bojan.istvancic@gmail.com>
2023-03-19 20:12:00 +0100

    add readingt json middleware, add some random display for / route, add not found and error middleware, spint the app and connect to db

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('04 Store API')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+const connectDB = require("./db/connect");
+
+app.use(express.json());
+// middleware
+
+app.get("/", (req, res) => {
+  res.send("<h1>Store API</h1><a href='/api/v1/products'>Products Routes</a>");
+});
+// routes
+
+app.use(notFoundMiddleware);
+// not found route
+app.use(errorMiddleware);
+// error route
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    // conenct db
+    app.listen(port, console.log(`Server is listening to the port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();

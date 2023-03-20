0598ad2d3973fa8ad791302a4fc38da7e0444463
BojanIstvancic <bojan.istvancic@gmail.com>
2023-03-20 21:07:07 +0100

    add selection to certain fields

--- controllers/products.js
+++ controllers/products.js
@@ -2,8 +2,8 @@ const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
   // this call is just for testing purposes
-  const products = await Product.find({}).sort("-name price");
-  a;
+  const products = await Product.find({}).select("name price");
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -13,13 +13,14 @@ PRACTICE FILTERING AND SORT
 - sort - sort data based on some criteria "ascending / descending"
 */
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   // get the query parameter
   /*
   create URI with query parameters
 
   http://localhost:3000/api/v1/products?name=ed - ?name=ed -> filter all products that contain "ed" in their "name"
   http://localhost:3000/api/v1/products?name=ed&sort=-name - &sort=-name -> -name  -> sort all products using "name" parameter "descending"
+  http://localhost:3000/api/v1/products?fields=name,price - select just certain fields (name, price)
   */
   const queryObject = {};
 
@@ -44,11 +45,19 @@ const getAllProducts = async (req, res) => {
   if (sort) {
     const sortList = sort.split(",").join(" ");
     // split into the array and join together
+    // name,-price -> name -price
     result = result.sort(sortList);
     // add this sorting parameter if any kind of sort is added
+    //.sort("name -price") - used for sorting
   } else {
     result = result.sort("createdAt");
     // if no sort is added automatically sory by "createdAt ascending"
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+    //.slect("name price") - select just certain fields
   }
 
   const products = await result;

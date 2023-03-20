ba394d46f42072508daa49dc669ab8b6c0cba8f2
BojanIstvancic <bojan.istvancic@gmail.com>
2023-03-20 09:00:59 +0100

    create products route,add basic controller functions for products route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
 const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
 app.use(express.json());
 // middleware
@@ -12,6 +13,8 @@ app.get("/", (req, res) => {
   res.send("<h1>Store API</h1><a href='/api/v1/products'>Products Routes</a>");
 });
 // routes
+
+app.use("/api/v1/products", productsRouter);
 
 app.use(notFoundMiddleware);
 // not found route

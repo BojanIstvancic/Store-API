b543eb6ecc750f56ab67f4a1966bac4ae5615d82
BojanIstvancic <bojan.istvancic@gmail.com>
2023-03-20 09:22:32 +0100

    create products schema

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
+require("express-async-errors");
+
 const express = require("express");
 const app = express();
-const notFoundMiddleware = require("./middleware/not-found");
-const errorMiddleware = require("./middleware/error-handler");
+
 const connectDB = require("./db/connect");
 const productsRouter = require("./routes/products");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // middleware

0b41a53cc54cc216201eb1bc8adf7ed960b11fbd
BojanIstvancic <bojan.istvancic@gmail.com>
2023-03-20 21:26:52 +0100

    add page/limit/skip filter

--- controllers/products.js
+++ controllers/products.js
@@ -21,6 +21,9 @@ const getAllProducts = async (req, res) => {
   http://localhost:3000/api/v1/products?name=ed - ?name=ed -> filter all products that contain "ed" in their "name"
   http://localhost:3000/api/v1/products?name=ed&sort=-name - &sort=-name -> -name  -> sort all products using "name" parameter "descending"
   http://localhost:3000/api/v1/products?fields=name,price - select just certain fields (name, price)
+ {{URL}}/products?fields=name,price&sort=name&page=3 - logic is below (skip/limit)
+ {{URL}}/products?fields=name,price&sort=name&limit=3 - logic is below (limit only)
+
   */
   const queryObject = {};
 
@@ -59,6 +62,16 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldsList);
     //.slect("name price") - select just certain fields
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10; // even if we don't have pagination we still can work with limit only
+  const skip = (page - 1) * limit;
+  // 10 10 3
+  // page 1 = skip 0 - skip 0 items, display 10 items
+
+  result = result.skip(skip).limit(limit);
+  // .skip() - amount of items to skip - like pagination
+  // .limit() - amount of items to be returned
 
   const products = await result;
   // then setup sort
